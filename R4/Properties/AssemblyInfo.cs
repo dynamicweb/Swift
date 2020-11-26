4712123df4e4db58f87c33909ccaecd69a8c163c
Stefan Winther <swi@dynamicweb.dk>
2020-11-26 11:39:29 +0100

    Initial commit

--- R4/Properties/AssemblyInfo.cs
+++ R4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rapido4ProtoTypeFiles")]
+[assembly: AssemblyTitle("R4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rapido4ProtoTypeFiles")]
+[assembly: AssemblyProduct("R4")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
