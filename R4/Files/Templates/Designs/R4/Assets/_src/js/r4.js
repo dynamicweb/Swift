02e4db65cf4bc973cb09effbc949c0e53986aac4
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-02-22 15:34:01 +0100

    #656 - Move js

--- R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
+++ R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
@@ -3,9 +3,11 @@ import { tns } from 'tiny-slider/src/tiny-slider';
 import { Sliders } from './_sliders';
 import { hcOffcanvasNav } from 'hc-offcanvas-nav';
 import "regenerator-runtime/runtime";
+import { Typeahead } from './_typeahead';
 
 window.tns = tns;
 window.Sliders = Sliders;
+window.Typeahead = Typeahead;
 
 document.addEventListener('DOMContentLoaded', function (event) {
     Sliders.Init();
