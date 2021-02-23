0ba2473de3cfb1ed1dbe28557a0440392b63991b
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-02-23 11:13:21 +0100

    #766 - Cleanup

--- R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
+++ R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
@@ -3,11 +3,9 @@ import { tns } from 'tiny-slider/src/tiny-slider';
 import { Sliders } from './_sliders';
 import { hcOffcanvasNav } from 'hc-offcanvas-nav';
 import "regenerator-runtime/runtime";
-import { Typeahead } from './_typeahead';
 
 window.tns = tns;
 window.Sliders = Sliders;
-window.Typeahead = Typeahead;
 
 document.addEventListener('DOMContentLoaded', function (event) {
     Sliders.Init();
