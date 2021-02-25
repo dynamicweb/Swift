5a43441489d3fc8d6c4615caec8866eab941a952
Stefan Winther <swi@dynamicweb.dk>
2021-02-25 15:23:11 +0100

    #295 - added _typeahead js file

--- R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
+++ R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
@@ -1,12 +1,19 @@
 import { Collapse, Dropdown } from 'bootstrap';
 import { tns } from 'tiny-slider/src/tiny-slider';
 import { Sliders } from './_sliders';
+import { Typeahead } from './_typeahead';
 import { hcOffcanvasNav } from 'hc-offcanvas-nav';
 import "regenerator-runtime/runtime";
 
 window.tns = tns;
 window.Sliders = Sliders;
+window.Typeahead = Typeahead;
 
 document.addEventListener('DOMContentLoaded', function (event) {
-    Sliders.Init();
+    Sliders.init();
+	Typeahead.init();
 });
+
+window.onpopstate = function (event) {
+	Typeahead.navigateToPage(document.location.href);
+};
