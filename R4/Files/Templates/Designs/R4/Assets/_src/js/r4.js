a3a8933fca5b629ae3a90556fb85733f9204209a
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-03-18 14:02:34 +0100

    #1255 - Remove lazyloadimg js

--- R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
+++ R4/Files/Templates/Designs/R4/Assets/_src/js/r4.js
@@ -5,7 +5,6 @@ import { Sliders } from './_sliders';
 import { Cart } from './_cart';
 import { ProductList } from './_productlist';
 
-import { Lazyimage } from './_lazyimage';
 import { Typeahead } from './_typeahead';
 import { hcOffcanvasNav } from 'hc-offcanvas-nav';
 
@@ -16,14 +15,12 @@ window.Sliders = Sliders;
 window.Cart = Cart;
 window.ProductList = ProductList;
 window.Typeahead = Typeahead;
-window.Lazyimage = Lazyimage;
 
 
 document.addEventListener('DOMContentLoaded', function (event) {
  
     Sliders.init();
 	Typeahead.init();
-	Lazyimage.Init();
 });
 
 window.onpopstate = function (event) {
