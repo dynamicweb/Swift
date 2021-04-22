f99fe11a8f48c9ecb05c35ab5aeb8a22fb57536d
Steffen Jørgensen <stj@dynamicweb.dk>
2021-04-22 08:34:05 +0200

    Merge branch 'main' into feature/f453-wcag/u647-wcag (Re-added missing conditional)

--- R4/Files/Templates/Designs/R4/Assets/_src/js/_sliders.js
+++ R4/Files/Templates/Designs/R4/Assets/_src/js/_sliders.js
@@ -31,28 +31,30 @@ const Sliders = function() {
 						itemsInSlider = 1;
 					}
 
-					var slider = tns({
-						container: sliderContainer,
-						controls: false,
-						items: 1.2,
-						gutter: 16,
-						loop: false,
-						rewind: false,
-						arrowKeys: false,
-						lazyload: true,
-						slideBy: "page",
-						responsive: {
-							992: {
-								items: itemsInSlider,
-								controls: true,
-								controlsText: [
-									'<div class="tns-controls-icon" style="height:3em; width: 3em;"><span class="icon-3"><svg viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg></span></div>',
-									'<div class="tns-controls-icon" style="height:3em; width: 3em;"><span class="icon-3"><svg viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg></span></div>'],
-							}
-						},
-						mouseDrag: true,
-						navPosition: 'bottom'
-					});
+					if (sliderIsInitialized == false) {
+						var slider = tns({
+							container: sliderContainer,
+							controls: false,
+							items: 1.2,
+							gutter: 16,
+							loop: false,
+							rewind: false,
+							arrowKeys: false,
+							lazyload: true,
+							slideBy: "page",
+							responsive: {
+								992: {
+									items: itemsInSlider,
+									controls: true,
+									controlsText: [
+										'<div class="tns-controls-icon" style="height:3em; width: 3em;"><span class="icon-3"><svg viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg></span></div>',
+										'<div class="tns-controls-icon" style="height:3em; width: 3em;"><span class="icon-3"><svg viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg></span></div>'],
+								}
+							},
+							mouseDrag: true,
+							navPosition: 'bottom'
+						});
+					}
 				}
 			} 
         }
