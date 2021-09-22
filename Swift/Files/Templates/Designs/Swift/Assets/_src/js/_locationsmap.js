1aa88a0ca6e70592d4bab72039100e4b4c6d272e
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-09-22 15:31:13 +0200

    #4151 - Script cleanup and optimization

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_locationsmap.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_locationsmap.js
@@ -1,50 +1,49 @@
 const LocationsMap = function () {
+	var map;
 	var markers = [];
 	var markersCount = 0;
-	var defaultLat = 0;
-	var defaultLng = 0;
-	var initialZoomLevel = 4;
+	var infoWindow;
 
-	var directionsLabel = "Directions";
-	var noLocationsLabel = "No options available in the selected area";
-
-	var infoWindow = new google.maps.InfoWindow({
-		maxWidth: 300,
-		minWidth: 220
-	});
+	var settings = {
+		locations: [],
+		mapStyle: [],
+		defaultLat: 0.0,
+		defaultLng: 0.0,
+		initialZoomLevel: 4,
+		regionCode: "DK",
+		directionsLabel: "Directions",
+		noLocationsFoundLabel: "No options available in the selected area"
+	};
 
 	return {
-		init: function () {
-			// Set the geocoder
-			geocoder = new google.maps.Geocoder();
+		init: function (mapSettings) {
+			settings = mapSettings;
 
 			// Set initial map center
 			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(showCurrentPosition);
+				navigator.geolocation.getCurrentPosition(LocationsMap.showCurrentPosition);
 			} else {
 				console.log("Geolocation is not supported by this browser");
 			}
 
-			const myMapCenter = {
-				lat: defaultLat,
-				lng: defaultLng
-			};
-
-			// Create the map
-			var map = new google.maps.Map(document.getElementById('map'), {
-				center: myMapCenter,
-				zoom: initialZoomLevel,
+			// Create the map instance
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: {
+					lat: settings.defaultLat,
+					lng: settings.defaultLng
+				},
+				zoom: settings.initialZoomLevel,
 				disableDefaultUI: true,
 				zoomControl: true,
 				zoomControlOptions: {
 					position: google.maps.ControlPosition.RIGHT_TOP,
 				},
-				styles: mapStyle
+				styles: settings.mapStyle
 			});
 
-			// Markup map
-			locations.forEach(function (location) {
-				marklocation(location);
+			// Add markers
+			settings.locations.forEach(function (location) {
+				LocationsMap.markLocation(location);
 				markersCount++;
 			});
 
@@ -53,12 +52,24 @@ const LocationsMap = function () {
 				var locationsListElement = document.querySelector("#locationsList");
 				locationsListElement.innerHTML = "";
 
-				updateLocationsList();
+				LocationsMap.updateLocationsList();
 			});
 
-			//Search on map (geocode)
-			document.querySelector("#mapSearchButton").addEventListener("click", function (event) {
-				geocode({ address: event.currentTarget.value })
+			// Info window
+			infoWindow = new google.maps.InfoWindow({
+				maxWidth: 300,
+				minWidth: 220
+			});
+
+			// Search on map (geocode)
+			document.querySelectorAll(".js-map-search-bar").forEach(function (searchBar) {
+				searchBar.addEventListener("submit", function (event) {
+					event.preventDefault();
+
+					var searchField = event.currentTarget.querySelector("[type=\"text\"]");
+
+					LocationsMap.geocode({ 'address': searchField.value.replace(" ", "+"), 'region': settings.regionCode });
+				});
 			});
 		},
 
@@ -72,7 +83,7 @@ const LocationsMap = function () {
 		},
 
 		// Create a marker and set its position
-		marklocation: function(location){
+		markLocation: function(location){
 			var marker = new google.maps.Marker({
 				icon: {
 					path: "M4,6C2.9,6,2,5.1,2,4s0.9-2,2-2s2,0.9,2,2S5.1,6,4,6 M4,0C1.8,0,0,1.8,0,4c0,3,4,7.4,4,7.4S8,7,8,4C8,1.8,6.2,0,4,0z",
@@ -89,14 +100,13 @@ const LocationsMap = function () {
 
 			// show location info when marker is clicked
 			marker.addListener('click', function () {
-				openInfo(marker);
+				LocationsMap.openInfo(marker);
 			});
 
 			markers.push(marker); // Keep marker instances in a global array
-
 		},
 
-		//Update the location list based on the visible area on the map
+		// Update the location list based on the visible area on the map
 		updateLocationsList: function() {
 			var bounds = map.getBounds();
 			var markersFound = 0;
@@ -105,7 +115,7 @@ const LocationsMap = function () {
 				var marker = markers[i];
 
 				if (bounds.contains(marker.getPosition()) === true) {
-					renderLocationListItem(i, "AddToList");
+					LocationsMap.renderLocationListItem(i, "AddToList");
 					markersFound++;
 				}
 			}
@@ -117,17 +127,17 @@ const LocationsMap = function () {
 				notificationElement.classList.add("alert");
 				notificationElement.classList.add("alert-info");
 				notificationElement.setAttribute("role", "alert");
-				notificationElement.innerHTML = noLocationsLabel;
+				notificationElement.innerHTML = settings.noLocationsFoundLabel;
 				locationsListElement.appendChild(notificationElement);
 			}
 		},
 
-		//Render the list item
+		// Render the list item
 		renderLocationListItem: function(id, type) {
 			var target = document.querySelector("#locationsList");
 
-			//Data
-			var location = locations[id];
+			// Data
+			var location = settings.locations[id];
 			var name = location.company != "" ? location.company : location.name;
 			var addressLineOne = location.address;
 			var addressLineTwoArr = [];
@@ -154,7 +164,7 @@ const LocationsMap = function () {
 			}
 			var addressLineThree = addressLineThreeArr.join(", ");
 
-			//List element
+			// List element
 			var listÌtemElement = document.createElement('div');
 			listÌtemElement.style.cursor = "pointer";
 			listÌtemElement.className = "list-group-item";
@@ -163,7 +173,7 @@ const LocationsMap = function () {
 			containerElement.className = "d-flex";
 			listÌtemElement.appendChild(containerElement);
 
-			//Left column
+			// Left column
 			var columnOneElement = document.createElement('div');
 			columnOneElement.className = "flex-fill";
 
@@ -192,14 +202,14 @@ const LocationsMap = function () {
 
 			containerElement.appendChild(columnOneElement);
 
-			//Directions - In list
+			// Directions - In list
 			if (type == "AddToList" && addressLineTwo != "") {
 				var columnTwoElement = document.createElement('div');
 
 				var directionsElement = document.createElement('a');
 				directionsElement.className = "btn icon-2 pe-0 pt-0";
 				directionsElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-corner-up-right\"><polyline points=\"15 14 20 9 15 4\"></polyline><path d=\"M4 20v-7a4 4 0 0 1 4-4h12\"></path></svg>";
-				directionsElement.title = directionsLabel;
+				directionsElement.title = settings.directionsLabel;
 				directionsElement.href = "https://www.google.dk/maps/dir//" + addressLineTwoArr.join("+");
 				directionsElement.target = "_blank";
 				columnTwoElement.appendChild(directionsElement);
@@ -207,12 +217,12 @@ const LocationsMap = function () {
 				containerElement.appendChild(columnTwoElement);
 			}
 
-			//Directions in info
+			// Directions in info
 			if (type == "UpdateInfo" && addressLineTwo != "") {
 				var directionsElement = document.createElement('a');
 				directionsElement.className = "btn btn-primary w-100 mt-3";
-				directionsElement.innerHTML = directionsLabel;
-				directionsElement.title = directionsLabel;
+				directionsElement.innerHTML = settings.directionsLabel;
+				directionsElement.title = settings.directionsLabel;
 				directionsElement.href = "https://www.google.dk/maps/dir//" + addressLineTwoArr.join("+");
 				directionsElement.target = "_blank";
 				listÌtemElement.appendChild(directionsElement);
@@ -220,7 +230,7 @@ const LocationsMap = function () {
 
 			if (type == "AddToList") {
 				listÌtemElement.setAttribute("data-location-number", id);
-				listÌtemElement.addEventListener("click", focusOnMarker);
+				listÌtemElement.addEventListener("click", LocationsMap.focusOnMarker);
 				target.appendChild(listÌtemElement);
 			} else if (type == "UpdateInfo") {
 				var contentString = listÌtemElement.innerHTML;
@@ -228,7 +238,7 @@ const LocationsMap = function () {
 			}
 		},
 
-		//Move the map to the selected marker location
+		// Move the map to the selected marker location
 		focusOnMarker: function (event) {
 			infoWindow.close();
 
@@ -238,35 +248,29 @@ const LocationsMap = function () {
 			map.setZoom(7);
 			map.panTo(selectedMarker.getPosition());
 
-			clearListItemSelection();
-
-			event.currentTarget.classList.add("active");
-
-			openInfo(selectedMarker)
+			LocationsMap.clearListItemSelection();
+			LocationsMap.openInfo(selectedMarker)
 		},
 
-		//Click on marker
+		// Click on marker
 		openInfo: function (marker) {
-			renderLocationListItem(marker.id, "UpdateInfo");
+			LocationsMap.renderLocationListItem(marker.id, "UpdateInfo");
 			infoWindow.open(map, marker);
 
 			map.panTo(marker.getPosition());
 		},
 
-		//Clear list item selection
+		// Clear list item selection
 		clearListItemSelection: function () {
 			document.querySelectorAll("[data-location-number]").forEach(function (locationItem) {
 				locationItem.classList.remove("active");
 			});
 		},
 
-		//Get position from address and move the map to the location
+		// Get position from address and move the map to the location
 		geocode: function (request) {
 			var geocoder = new google.maps.Geocoder();
-			var address = document.querySelector("#mapSearch").value;
-			encodeURI(address.replace(" ", "+"));
-
-			geocoder.geocode({ 'address': address, 'region': '@(regionCode)' }, function (results, status) {
+			geocoder.geocode(request, function (results, status) {
 				if (status == google.maps.GeocoderStatus.OK) {
 					map.setZoom(14);
 					map.panTo(results[0].geometry.location);
