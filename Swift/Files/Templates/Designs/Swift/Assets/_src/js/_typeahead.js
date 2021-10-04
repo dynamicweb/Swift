7b50b72cd4d66dc0299490a30a9bb33087d27821
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-10-04 13:04:18 +0200

    #4562 - Autofocus on field when pre-select

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_typeahead.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_typeahead.js
@@ -121,7 +121,8 @@ const Typeahead = function() {
 			var suggestionElement = elm.querySelector(".js-suggestion");
 
 			document.querySelectorAll(".js-type-ahead-field").forEach(function (field) {
-				field.value = suggestionElement.innerText;
+				field.value = suggestionElement.innerText + " ";
+				field.focus();
 
 				var formElm = field.closest(".js-suggest-form");
 				var parm = formElm.querySelector(".js-type-ahead-parameter");
