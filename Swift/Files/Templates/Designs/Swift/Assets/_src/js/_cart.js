50e67623d1597d4f893dec978afc4909920bb3d8
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-10-12 10:58:56 +0200

    #4740 - Basic variant list implementation

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_cart.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_cart.js
@@ -25,10 +25,10 @@ const Cart = function () {
 
 			if (globalDispatcher != false && localDispatcher != false) {
 				//UI updates
-				var clickedButtonWidth = clickedButton.offsetWidth + "px";
+				var clickedButtonWidth = clickedButton.width + "px";
 
 				clickedButton.setAttribute("data-content", clickedButton.innerHTML);
-				clickedButton.style.minWidth = clickedButtonWidth;
+				clickedButton.style.width = clickedButtonWidth;
 				clickedButton.classList.add("disabled");
 				clickedButton.disabled = true;
 				clickedButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><title>circle-notch</title><g fill="#ffffff"><path d="M288 24.103v8.169a11.995 11.995 0 0 0 9.698 11.768C396.638 63.425 472 150.461 472 256c0 118.663-96.055 216-216 216-118.663 0-216-96.055-216-216 0-104.534 74.546-192.509 174.297-211.978A11.993 11.993 0 0 0 224 32.253v-8.147c0-7.523-6.845-13.193-14.237-11.798C94.472 34.048 7.364 135.575 8.004 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.789 504 256c0-121.187-86.924-222.067-201.824-243.704C294.807 10.908 288 16.604 288 24.103z"></path></g></svg>';
@@ -67,6 +67,7 @@ const Cart = function () {
 			if (globalDispatcher != false && localDispatcher != false) {
 				//Cleanup
 				clickedButton.classList.remove("disabled");
+				clickedButton.style.width = "auto";
 				clickedButton.disabled = false;
 				clickedButton.innerHTML = clickedButton.getAttribute("data-content");
 				clickedButton.setAttribute("data-content", "");
@@ -94,6 +95,7 @@ const Cart = function () {
 			}, 200);
 
 			clickedButton.classList.remove("disabled");
+			clickedButton.style.width = "auto";
 			clickedButton.disabled = false;
 			clickedButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><title>circle-notch</title><g fill="#ffffff"><path d="M288 24.103v8.169a11.995 11.995 0 0 0 9.698 11.768C396.638 63.425 472 150.461 472 256c0 118.663-96.055 216-216 216-118.663 0-216-96.055-216-216 0-104.534 74.546-192.509 174.297-211.978A11.993 11.993 0 0 0 224 32.253v-8.147c0-7.523-6.845-13.193-14.237-11.798C94.472 34.048 7.364 135.575 8.004 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.789 504 256c0-121.187-86.924-222.067-201.824-243.704C294.807 10.908 288 16.604 288 24.103z"></path></g></svg>';
 		}
