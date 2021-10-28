8c931b12fd8a0e26e19ebe7baf5452592af175e0
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2021-10-28 09:43:01 +0200

    #5397 - Simplify the setup + Do not update the whole page on change

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_favorite.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_favorite.js
@@ -1,27 +1,86 @@
 const Favorite = function () {
 
 	return {
-		Add: function (e) {
+		Update: async function (e) {
 			var clickedButton = e.currentTarget;
 			var form = clickedButton.closest('form');
+
+			var command = clickedButton.getAttribute('data-command');
+			command = command == "add" ? "addproducttofavoritelist" : command;
+			command = command == "remove" ? "removeproductfromfavoritelist" : command;
 			form.querySelector('[name="FavoriteListId"]').value = clickedButton.getAttribute('data-list-id');
-			form.querySelector('[name="FavoriteCmd"]').value = "addproducttofavoritelist";
-			swift.PageUpdater.Update(form);
+			form.querySelector('[name="FavoriteCmd"]').value = command;
+
+			if (command == "addproducttofavoritelist") {
+				clickedButton.setAttribute('data-command', 'remove');
+				clickedButton.querySelector('.js-filled-favorite-icon').classList.remove('d-none');
+				clickedButton.querySelector('.js-outline-favorite-icon').classList.add('d-none');
+			} else {
+				clickedButton.setAttribute('data-command', 'add');
+				clickedButton.querySelector('.js-filled-favorite-icon').classList.add('d-none');
+				clickedButton.querySelector('.js-outline-favorite-icon').classList.remove('d-none');
+			}
+
+			if (clickedButton.closest('form').querySelector('.js-any-filled-favorite-icon') != null) {
+				var favoriteFound = false;
+				clickedButton.closest('form').querySelectorAll('.js-filled-favorite-icon').forEach(function (icon) {
+					if (!icon.classList.contains('d-none')) {
+						favoriteFound = true;
+					}
+				});
+
+				if (favoriteFound) {
+					clickedButton.closest('form').querySelector('.js-any-filled-favorite-icon').classList.remove('d-none');
+					clickedButton.closest('form').querySelector('.js-any-outline-favorite-icon').classList.add('d-none');
+				} else {
+					clickedButton.closest('form').querySelector('.js-any-filled-favorite-icon').classList.add('d-none');
+					clickedButton.closest('form').querySelector('.js-any-outline-favorite-icon').classList.remove('d-none');
+				}
+			}
+
+			let formData = new FormData(form);
+			var fetchOptions = {
+				method: 'POST',
+				body: formData
+			};
+
+			//Fire the 'update' event
+			let event = new CustomEvent("update.swift.favorite", {
+				cancelable: true,
+				detail: {
+					formData: formData,
+					parentEvent: e
+				}
+			});
+			var globalDispatcher = document.dispatchEvent(event);
+			var localDispatcher = clickedButton.dispatchEvent(event);
+
+			if (globalDispatcher != false && localDispatcher != false) {
+				let response = await fetch(form.action, fetchOptions);
+
+				if (response.ok) {
+					Favorite.Success(response, formData, clickedButton);
+				} else {
+					Favorite.Error(response);
+				}
+			}
 		},
 
-		Remove: function (e) {
-			var clickedButton = e.currentTarget;
-			var form = clickedButton.closest('form');
-			form.querySelector('[name="FavoriteListId"]').value = clickedButton.getAttribute('data-list-id');
-			form.querySelector('[name="FavoriteCmd"]').value = "removeproductfromfavoritelist";
-			swift.PageUpdater.Update(form);
-		},
-
-		Success: function () {
+		Success: async function (response, formData, clickedButton) {
 			var favoriteNotification = document.querySelector("#favoriteNotificationToast");
 
-			if (favoriteNotification && event.detail.formData) {
-				var data = Object.fromEntries(event.detail.formData.entries());
+			//Fire the 'updated'´event
+			let event = new CustomEvent("updated.swift.cart", {
+				cancelable: true,
+				detail: {
+					formData: formData
+				}
+			});
+			var globalDispatcher = document.dispatchEvent(event);
+			var localDispatcher = clickedButton.dispatchEvent(event);
+
+			if (favoriteNotification && globalDispatcher != false && localDispatcher != false) {
+				var data = Object.fromEntries(formData.entries());
 				var command = data.FavoriteCmd;
 
 				if (command != null) {
@@ -36,6 +95,10 @@ const Favorite = function () {
 					}
 				}
 			}
+		},
+
+		Error: function (e) {
+
 		}
 	}
 }();
