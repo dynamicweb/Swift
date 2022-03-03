f39266957f93fefb6e7d4c4514cee64d1f850a7b
DYNAMIC-SYSTEMS\kth <kth@dynamicweb.dk>
2022-03-03 10:14:24 +0100

    #6941 - Mini download cart counter

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_cart.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_cart.js
@@ -32,9 +32,13 @@ const Cart = function () {
 				clickedButton.disabled = true;
 				clickedButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><title>circle-notch</title><g fill="#ffffff"><path d="M288 24.103v8.169a11.995 11.995 0 0 0 9.698 11.768C396.638 63.425 472 150.461 472 256c0 118.663-96.055 216-216 216-118.663 0-216-96.055-216-216 0-104.534 74.546-192.509 174.297-211.978A11.993 11.993 0 0 0 224 32.253v-8.147c0-7.523-6.845-13.193-14.237-11.798C94.472 34.048 7.364 135.575 8.004 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.789 504 256c0-121.187-86.924-222.067-201.824-243.704C294.807 10.908 288 16.604 288 24.103z"></path></g></svg>';
 
-				document.querySelectorAll(".js-cart-qty").forEach(function (el) {
-					el.classList.add("mini-cart-quantity-added");
-				});
+				if (formData.get("minicartid") == null) {
+					document.querySelectorAll(".js-cart-qty").forEach(function (el) {
+						el.classList.add("mini-cart-quantity-added");
+					});
+				} else {
+					document.querySelector("#Cart_" + formData.get("minicartid")).classList.add("mini-cart-quantity-added");
+				}
 
 				//Fetch
 				let response = await fetch(form.action, fetchOptions);
@@ -72,16 +76,25 @@ const Cart = function () {
 				clickedButton.setAttribute("data-content", "");
 
 				var removeFocusCssClassTimer = setTimeout(function () {
-					document.querySelectorAll(".js-cart-qty").forEach(function (el) {
-						el.classList.remove("mini-cart-quantity-added");
-					});
+					if (formData.get("minicartid") == null) {
+						document.querySelectorAll(".js-cart-qty").forEach(function (el) {
+							el.classList.remove("mini-cart-quantity-added");
+						});
+					} else {
+						document.querySelector("#Cart_" + formData.get("minicartid")).classList.remove("mini-cart-quantity-added");
+					}
 				}, 200);
 
 				//Replace the markup
 				var totalQuantity = html != undefined ? html : 0;
-				document.querySelectorAll(".js-cart-qty").forEach(function (el) {
-					el.innerHTML = "(" + totalQuantity.trim() + ")";
-				})
+
+				if (formData.get("minicartid") == null) {
+					document.querySelectorAll(".js-cart-qty").forEach(function (el) {
+						el.innerHTML = "(" + totalQuantity.trim() + ")";
+					})
+				} else {
+					document.querySelector("#Cart_" + formData.get("minicartid")).innerHTML = "(" + totalQuantity.trim() + ")";
+				}
 			}
 		},
 
