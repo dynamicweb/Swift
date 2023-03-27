90c1ab77c0ad3019bf96c3cfe6641f87a14c1558
NunoAguiar <nunolaguiar@hotmail.com>
2023-03-27 16:36:47 +0100

    Add Back in Stock Notification

---
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_backInStockNotification.js
@@ -0,0 +1,23 @@
+const BackInStockNotification = function (){
+	return{
+		SubmitNotification(formId, modalId, notifiedMessageId, notificationButtonId){
+			const form = document.getElementById(formId);
+			const formData = new FormData(form);
+			const modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
+			const notifiedMessage = document.getElementById(notifiedMessageId);
+			const notificationButton = document.getElementById(notificationButtonId);
+
+			fetch(form.getAttribute("action"), {
+				method: 'POST',
+				body: formData
+			})
+				.then(function () {
+					modal.hide();
+					notifiedMessage.classList.remove("d-none");
+					notificationButton.classList.add("d-none");
+				});
+		}
+	}
+}();
+
+export { BackInStockNotification };
