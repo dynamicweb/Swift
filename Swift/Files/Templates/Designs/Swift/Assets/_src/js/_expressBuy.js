f042853ff3de26e1ec151fff06f138090f8ffbf2
NunoAguiar <nunolaguiar@hotmail.com>
2023-05-09 09:46:39 +0100

    Code review change

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_expressBuy.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_expressBuy.js
@@ -1,39 +1,38 @@
 const ExpressBuy = function (){
 	return {
 		OpenReplacementModal: function(productCounter){
-			const modalBody = document.querySelector("#replacementProductsModal .modal-body");
-			const productArticle = document.querySelector("article[data-product-counter='" + productCounter + "']");
+			const modalBody = document.querySelector(".js-replacement-modal-body");
+			const productArticle = document.querySelector(".js-replacement-product[data-product-counter='" + productCounter + "']");
 			const productReplacementFeed = productArticle.getAttribute("data-replacement-feed");
 	
 			swift.PageUpdater.UpdateFromUrl(modalBody, productReplacementFeed).then(function() {
-				const productNameElement = productArticle.querySelector(".productName");
-				const modalTitleProductName = document.querySelector("#modalTitleProductName");
+				const productNameElement = productArticle.querySelector(".js-product-name");
+				const modalTitleProductName = document.querySelector(".js-modal-title-product-name");
 	
 				modalTitleProductName.innerHTML = productNameElement.innerHTML
-				sessionStorage.setItem("productCounter", productCounter);
+				sessionStorage.setItem("js-product-counter", productCounter);
 			});
 		},
 	
 		AcceptReplacementProduct: function (){
-			const productCounter = sessionStorage.getItem("productCounter");
-			const replacementArticle = document.querySelector("#replacementProductsModal .modal-body article");
-			const productArticle = document.querySelector("article[data-product-counter='" + productCounter + "']");
+			const productCounter = sessionStorage.getItem("js-product-counter");
+			const replacementArticle = document.querySelector(".js-replacement-modal-body .js-replacement-product");
+			const productArticle = document.querySelector(".js-replacement-product[data-product-counter='" + productCounter + "']");
 			
 			productArticle.replaceWith(replacementArticle);
 			this.UpdateReplacementElements(productCounter, true);
 		},
 	
 		RefuseReplacementProduct: function () {
-			const productCounter = sessionStorage.getItem("productCounter");
-			this.UpdateReplacementElements(sessionStorage.getItem("productCounter"), false);
+			this.UpdateReplacementElements(sessionStorage.getItem("js-product-counter"), false);
 		},
 	
 		UpdateReplacementElements: function(productCounter, replaced) {
-			document.querySelectorAll(".productReplacementMessage[data-product-counter='" + productCounter + "']").forEach(function (messageElement) {
+			document.querySelectorAll(".js-product-replacements-message[data-product-counter='" + productCounter + "']").forEach(function (messageElement) {
 				messageElement.innerHTML = replaced ? messageElement.getAttribute("data-replaced-by-message") : messageElement.getAttribute("data-replace-refused-message");
 			})
-			document.querySelector(".productReplacementsButton[data-product-counter='" + productCounter + "']").remove();
-			sessionStorage.removeItem("productCounter");
+			document.querySelector(".js-product-replacements-button[data-product-counter='" + productCounter + "']").remove();
+			sessionStorage.removeItem("js-product-counter");
 		}
 	}
 }();
