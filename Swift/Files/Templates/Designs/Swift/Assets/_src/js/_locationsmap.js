7d2b460b833023a619ce520eec305d1eeb8d0707
Karsten Thuen <kth@dynamicweb.dk>
2023-06-02 10:18:33 +0200

    #13758 - Add Address to directions link

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_locationsmap.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_locationsmap.js
@@ -246,7 +246,7 @@ const LocationsMap = function () {
 				directionsElement.className = "btn icon-2 pe-0 pt-0";
 				directionsElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-corner-up-right\"><polyline points=\"15 14 20 9 15 4\"></polyline><path d=\"M4 20v-7a4 4 0 0 1 4-4h12\"></path></svg>";
 				directionsElement.title = settings.directionsLabel;
-				directionsElement.href = "https://www.google.dk/maps/dir//" + addressLineTwoArr.join("+");
+				directionsElement.href = "https://www.google.dk/maps/dir//" + location.address + "+" + addressLineTwoArr.join("+");
 				directionsElement.target = "_blank";
 				columnTwoElement.appendChild(directionsElement);
 
@@ -259,7 +259,7 @@ const LocationsMap = function () {
 				directionsElement.className = "btn btn-primary w-100 mt-3";
 				directionsElement.innerHTML = settings.directionsLabel;
 				directionsElement.title = settings.directionsLabel;
-				directionsElement.href = "https://www.google.dk/maps/dir//" + addressLineTwoArr.join("+");
+				directionsElement.href = "https://www.google.dk/maps/dir//" + location.address + "+" + addressLineTwoArr.join("+");
 				directionsElement.target = "_blank";
 				listÌtemElement.appendChild(directionsElement);
 			}
