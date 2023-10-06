4c5df354ffbb63696019e3027c2e153ddd9b1b3c
Simon Martin Nielsen <smn@dynamicweb.dk>
2023-10-06 14:17:53 +0200

    #15035 #15510 - Delete thumbnails on video poster attribute. Add correct video path encoding, media fragments. Fixed small images in DetailsGallery not showing videos

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_video.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_video.js
@@ -6,11 +6,6 @@ const Video = function () {
 				var videoId = thumbnailElement.getAttribute("data-video-id");
 				swift.Video.setVimeoThumbnail(thumbnailElement, videoId);
 			});
-			document.querySelectorAll(".js-selfhosted-video-thumbnail").forEach(function (thumbnailElement) {
-				var videoUrl = thumbnailElement.getAttribute("data-src");
-				thumbnailElement.src = videoUrl;
-				swift.Video.getSelfHostedVideoPoster(thumbnailElement);
-			});
 		},
 
 		setVimeoThumbnail(element, videoId) {
@@ -26,25 +21,6 @@ const Video = function () {
 			})
 			.catch(error => {
 				console.log(error);
-			});
-		},
-		 getSelfHostedVideoPoster(video) {
-            
-			video.addEventListener('loadedmetadata', function () {
-				video.currentTime = 0.5;
-			});
-
-			video.addEventListener('seeked', function () {
-				var canvas = document.createElement('canvas');
-				canvas.width = 1920;
-				canvas.height = 1080;
-				var ctx = canvas.getContext('2d');
-				ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-				const dataUrl = canvas.toDataURL();
-
-				fetch(dataUrl).then(res => res.blob()).then(blob =>
-					video.poster = URL.createObjectURL(blob)
-				);
 			});
 		}
 	}
