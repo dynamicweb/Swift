eaabedc4cdbbbe449226f892822d101180e3afdb
smnDW <smn@dynamicweb.dk>
2024-02-28 11:03:18 +0100

    #17883 - ProductDetailsImage youtube link fix

--- Swift/Files/Templates/Designs/Swift/Assets/_src/js/_video.js
+++ Swift/Files/Templates/Designs/Swift/Assets/_src/js/_video.js
@@ -3,12 +3,20 @@ const Video = function () {
 	return {
 		init() {
 			document.querySelectorAll(".js-vimeo-video-thumbnail").forEach(function (thumbnailElement) {
-				var videoId = thumbnailElement.getAttribute("data-video-id");
-				swift.Video.setVimeoThumbnail(thumbnailElement, videoId);
+				const assetValue = thumbnailElement.dataset.assetValue;
+				swift.Video.setVimeoThumbnail(thumbnailElement, assetValue);
+			});
+			
+			document.querySelectorAll(".js-youtube-video-thumbnail").forEach(function (thumbnailElement) {
+				const assetValue = thumbnailElement.dataset.assetValue;
+				const size = thumbnailElement.dataset.size;
+				swift.Video.setYoutubeThumbnail(thumbnailElement, assetValue, size);
 			});
 		},
 
-		setVimeoThumbnail(element, videoId) {
+		setVimeoThumbnail(element, assetValue) {
+			const videoId = assetValue.substring(assetValue.lastIndexOf('/') + 1);
+	
 			fetch('https://vimeo.com/api/v2/video/' + videoId + '.json')
 			.then(function (response) {
 				return response.text();
@@ -22,6 +30,15 @@ const Video = function () {
 			.catch(error => {
 				console.log(error);
 			});
+		},
+		setYoutubeThumbnail(element, assetValue, size) {
+			const youtubeImageResolution = size ? size : 'hqdefault'; // maxresdefault, sddefault, hqdefault
+			const regex = /(?:youtube\.com\/.*[\?&]v=|youtu\.be\/)([\w-]+)/;
+			const match = assetValue.match(regex);
+			const videoId = match ? match[1] : '';
+			const youtubeThumbnail = `https://img.youtube.com/vi/${videoId}/${youtubeImageResolution}.jpg`;
+			element.src = youtubeThumbnail;
+			console.log("here", youtubeImageResolution, youtubeThumbnail)
 		}
 	}
 }();
