7bf2d76afb382466b7b883bc3d38cf266bb1803d
Stefan Winther <swi@dynamicweb.dk>
2024-04-10 12:36:43 +0200

    Rollup + pipeline cleanup + remove globalsettings.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import resolve from "@rollup/plugin-node-resolve";
 import eslint from "@rollup/plugin-eslint";
 import scss from "rollup-plugin-scss";
-import stylelint from 'rollup-plugin-stylelint';
+import stylelint from "rollup-plugin-stylelint";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import copy from "rollup-plugin-copy";
 const isProd = process.env.NODE_ENV === "production";
 const srcPath = "_src/";
 const distPath = "Files/Templates/Designs/Swift/Assets/";
+
+console.log("Building for " + (isProd ? "production" : "development"));
 
 export default [
   //Scripts
@@ -34,28 +36,28 @@ export default [
       // }),
       copy({
         targets: [
-          { src: 'node_modules/swiffy-slider/dist/', dest: distPath + 'lib/' },
-          { src: 'node_modules/htmx.org/dist/', dest: distPath + 'lib/' },
-          { src: 'node_modules/bootstrap/dist/', dest: distPath + 'lib/' },
-          { src: 'node_modules/aos/dist/', dest: distPath + 'lib/' },
-          { src: 'node_modules/plyr/dist/', dest: distPath + 'lib/' },
-          { src: 'node_modules/flatpickr/dist/', dest: distPath + 'lib/' },
-          { src: srcPath + 'js/test.js', dest: distPath }
+          { src: "node_modules/swiffy-slider/dist/", dest: distPath + "lib/" },
+          { src: "node_modules/htmx.org/dist/", dest: distPath + "lib/" },
+          { src: "node_modules/bootstrap/dist/", dest: distPath + "lib/" },
+          { src: "node_modules/aos/dist/", dest: distPath + "lib/" },
+          { src: "node_modules/plyr/dist/", dest: distPath + "lib/" },
+          { src: "node_modules/flatpickr/dist/", dest: distPath + "lib/" },
+          { src: srcPath + "js/test.js", dest: distPath },
         ],
         copyOnce: true,
         flatten: false,
-      })
+      }),
     ],
     output: [
       {
         file: distPath + "js/" + "swift.js",
         format: "umd",
-        name: "swift"
+        name: "swift",
       },
       {
         file: distPath + "js/" + "swift.esm.js",
         format: "es",
-        name: "swift"
+        name: "swift",
       },
     ],
   },
@@ -65,24 +67,21 @@ export default [
     plugins: [
       resolve(),
       stylelint.default({
-        include: srcPath + ['scss/**.scss'],
+        include: srcPath + ["scss/**.scss"],
         failOnError: true,
         report: true,
       }),
       scss({
         watch: srcPath + "scss",
-        verbose: true,
         outputStyle: isProd ? "compressed" : "expanded",
         sourceMap: isProd ? true : false,
         includePaths: ["node_modules"],
         fileName: "swift.css",
-        processor: css => postcss([autoprefixer])
-          .process(css)
-          .then(result => result.css),
+        processor: () => postcss([autoprefixer()]),
       }),
     ],
     output: {
-      file: distPath + "css/" + "swift.css"
-    }
+      file: distPath + "css/" + "swift.css",
+    },
   },
 ];
