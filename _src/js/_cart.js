a8dc2bef623b7198758fe90ac7735405bb2433a2
smnDW <smn@dynamicweb.dk>
2024-08-19 08:50:18 +0200

    #20393 - gtag correct unit price and discount

--- _src/js/_cart.js
+++ _src/js/_cart.js
@@ -7,6 +7,7 @@ const Cart = (function () {
   let productCurrency;
   let productReferer;
   let productPrice;
+  let productDiscount;
   let addQuantity;
   let isPendingQuote = "false";
 
@@ -25,8 +26,11 @@ const Cart = (function () {
       productCurrency = formData.get("ProductCurrency");
       productReferer = formData.get("ProductReferer");
       productPrice = formData.get("ProductPrice");
+      productDiscount = formData.get("ProductDiscount");
       addQuantity = formData.get("Quantity") ? formData.get("Quantity") : 1;
       isPendingQuote = formData.get("PendingQuote") ? formData.get("PendingQuote") : "false";
+      productPrice = productPrice !== null && productPrice !== "" ? parseFloat(productPrice) : productPrice;
+      productDiscount = productDiscount !== null && productDiscount !== "" ? parseFloat(productDiscount) : productDiscount;
 
       this.PushDataToGoogleAnalytics();
       let event = new CustomEvent("update.swift.cart", {
@@ -190,6 +194,7 @@ const Cart = (function () {
               currency: productCurrency,
               item_list_id: productReferer,
               price: productPrice,
+              discount: productDiscount,
               quantity: addQuantity,
             },
           ],
